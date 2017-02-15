fe5be357e13864b94754c5799c4c03961e1cb8b0
Jakub Stejskal <xstejs24@stud.fit.vutbr.cz>
2017-02-16 00:18:29 +0100

    Hlavičkový soubor

---
+++ pds-header.h
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   pds-header.h
+ * Author: frawless
+ *
+ * Created on 16. února 2017, 0:17
+ */
+
+#ifndef PDS_HEADER_H
+#define PDS_HEADER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//inkludované knihovny
+#include <ctype.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h> 
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+
+#include <netinet/ip6.h>
+#include <netinet/if_ether.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PDS_HEADER_H */
+
